66065db36eab29fbeb4c464a8ad0c18844d0404e
joskalenda <joskalenda3@gmail.com>
2022-03-11 23:05:27 +0200

    resolve deply

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/extensions */
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './component/Header';
